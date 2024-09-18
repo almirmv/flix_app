7838a44833e4362bf4a372621bb41ee5381844c5
almirmv <76816905+almirmv@users.noreply.github.com>
2024-09-17 23:36:10 -0300

    Aulas 15 a 17 implementados service e repository de Actors
    _ criado service de actors
    - criado repository de actors
    - alterada page de actors

--- actors/page.py
+++ actors/page.py
@@ -1,30 +1,44 @@
+from datetime import datetime
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
+from actors.service import ActorService
 
-
-actors = [
-    {
-        'id': 1,
-        'name': 'Leonardo Di Caprio'
-    },
-    {
-        'id': 2,
-        'name': 'Jim Carrey'
-    },
-    {
-        'id': 3,
-        'name': 'Robert de Niro'
-    },
-]
 def show_actors():
-    st.write('Lista de Atores/Atrizes')
-    AgGrid(
-        data=pd.DataFrame(actors),
-        key='actors_grid',
-    )
+    actor_service = ActorService()
+    actors = actor_service.get_actors()
 
-    st.title('Cdastrar novo Ator')
+    if actors:
+        st.write('Lista de Atores/Atrizes')
+        actors_df = pd.json_normalize(actors)
+        AgGrid(
+            data=actors_df,
+            key='actors_grid',
+        )
+    else:
+        st.warning('Nenhum Ator/Atriz encontrado')
+    
+    st.title('Cadastrar novo Ator')
     name = st.text_input("Nome do Ator")
+    birthday = st.date_input(
+        label="Data de Nascimento",
+        value=datetime.today(),
+        min_value=datetime(1600, 1, 1).date(),
+        max_value=datetime.today(),
+        format='DD/MM/YYYY', # altera apenas visualmente
+    )
+    nationality_dropdow = ['BRAZIL', 'USA']
+    nationality = st.selectbox(
+        label='Nacionalidade',
+        options=nationality_dropdow,
+    )
     if st.button('Cadastrar'):
-        st.success(f'Ator {name} cadastrado!')
+        new_actor = actor_service.create_actor(
+            name=name,
+            birthday=birthday,
+            nationality=nationality,
+        )
+        if new_actor:
+            st.rerun()
+        else:
+            st.error(f'Erro ao cadastrar Ator/Atriz. verifique os campos')
