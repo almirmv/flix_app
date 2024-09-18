91424e31c542b1493e651a840ec6dc0b4a4b3a4f
almirmv <76816905+almirmv@users.noreply.github.com>
2024-09-18 13:45:44 -0300

    Aulas 18 a 20 Service e Repository de movies
    - criado repository de movies
    - criado service de movies
    - alterada page movies para consumir o service

--- movies/page.py
+++ movies/page.py
@@ -1,27 +1,64 @@
 import pandas as pd
 import streamlit as st
+from datetime import datetime
 from st_aggrid import AgGrid
+from actors.service import ActorService
+from genres.service import GenreService
+from movies.service import MovieService
 
 
-
-movies = [
-    {
-        'id': 1,
-        'name': 'Matrix'
-    },
-    {
-        'id': 2,
-        'name': 'Rambo'
-    },
-    {
-        'id': 3,
-        'name': 'De volta para o futuro'
-    },
-]
 def show_movies():
-    st.write('Lista Filmes')
-    # aggrid so aceita dataframe, usamos pandas para converter
-    AgGrid(
-        data=pd.DataFrame(movies),
-        key='movies_grid',
+    movie_service = MovieService()
+    movies = movie_service.get_movies()
+    if movies:
+        st.write('Lista Filmes')
+        # aggrid so aceita dataframe, usamos pandas para converter
+        movies_df = pd.json_normalize(movies)
+        # remover coluna actors pois é multivalores, e remover id para elegancia        movies_df = movies_df.drop(columns=['actors', 'genre.id'])
+        AgGrid(
+            data=movies_df,
+            key='movies_grid',
+        )
+    else:
+        st.warning('Nenhum filme encontrado')
+    
+    # cadastrar novos filmes
+    st.title('Cadastrar novo Filme')
+    title = st.text_input('Título')
+    release_date = st.date_input(
+        label='Data de lançamento',
+        value=datetime.today(),
+        min_value=datetime(1800, 1, 1).date(),
+        max_value=datetime.today(),
+        format='DD/MM/YYYY',
     )
+    
+    # vai precisar carregar a lista de generos ---------
+    genre_service = GenreService()
+    genres = genre_service.get_genres()
+    # list compreesion
+    genres_names = {genre['name']:genre['id'] for genre in genres}
+    selected_genres_name = st.selectbox('Gênero', list(genres_names.keys()))
+    
+    # vai precisar carregar a lista de Atores ----------
+    actor_service = ActorService()
+    actors = actor_service.get_actors()
+    # list compreesion
+    actor_names = {actor['name']:actor['id'] for actor in actors}
+    selected_actors_names = st.multiselect('Atores/Atrizes', list(actor_names.keys()))
+    # outra lis compreensin para listar apenas ids ex. [1,3,7]
+    selected_actors_ids = [actor_names[name] for name in selected_actors_names]
+
+    resume = st.text_area('Resumo')
+    if st.button('Cadastrar'):
+        new_movie = movie_service.create_movie(
+            title=title,
+            release_date=release_date,
+            genre=genres_names[selected_genres_name],
+            actors=selected_actors_ids,
+            resume=resume,
+        )
+        if new_movie:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar filme. Verifique os campos')
---
+++ movies/repository.py
@@ -0,0 +1,46 @@
+
+import os
+import requests
+import streamlit as st
+from dotenv import load_dotenv
+from login.service import logout
+
+
+load_dotenv()
+
+
+class MovieRepository:
+
+    def __init__(self):
+        self.__base_url = os.environ['BASE_URL']
+        self.__movies_url = f'{self.__base_url}movies/'
+        self.__headers = {
+            'authorization': f'Bearer {st.session_state.token}'
+        }
+
+    def get_movies(self):
+        response = requests.get(
+            self.__movies_url,
+            headers=self.__headers,
+        )
+        if response.status_code == 200:
+            return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
+        
+        raise Exception(f'Erro ao obter dados da API. Status Code: {response.status_code}')
+    
+    def create_movie(self, movie):
+        response = requests.post(
+            self.__movies_url,
+            headers=self.__headers,
+            data=movie,
+        )
+        if response.status_code == 201:
+            return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
+        
+        raise Exception(f'Erro ao obter dados da API. Status Code: {response.status_code}')
