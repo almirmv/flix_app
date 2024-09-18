483d1327e3209fde39d2026a70df96bf5cb7587c
almirmv <76816905+almirmv@users.noreply.github.com>
2024-09-18 14:21:27 -0300

    Aulas 21 a 23 repository e service de Reviews
    - criado repository de reviews
    - criado service de reviews
    - alterada page de reviews

--- reviews/page.py
+++ reviews/page.py
@@ -1,27 +1,49 @@
 import pandas as pd
 import streamlit as st
+from movies.service import MovieService
+from reviews.service import ReviewService
 from st_aggrid import AgGrid
 
 
-
-reviews = [
-    {
-        'id': 1,
-        'stars': 5
-    },
-    {
-        'id': 2,
-        'stars': 4
-    },
-    {
-        'id': 3,
-        'stars': 3
-    },
-]
 def show_reviews():
-    st.write('Lista Avaliações')
-    # aggrid so aceita dataframe, usamos pandas para converter
-    AgGrid(
-        data=pd.DataFrame(reviews),
-        key='reviews_grid',
+    review_service = ReviewService()
+    reviews = review_service.get_reviews()
+    
+    if reviews:
+        st.write('Lista Avaliações')
+        reviews_df = pd.json_normalize(reviews)
+        # aggrid so aceita dataframe, usamos pandas para converter
+        AgGrid(
+            data=reviews_df,
+            key='reviews_grid',
+        )
+    else:
+        st.warning('Nenhuma avaliação encontrada')
+    
+    # cadastrar nova avaliação
+    st.title('Cadastrar nova avaliação')
+    movie_service = MovieService()
+    movies = movie_service.get_movies()
+    # para mostrar lista com nomes dos filmes e nao os ids
+    movie_titles = {movie['title']: movie['id'] for movie in movies}
+    selected_movie_title = st.selectbox('Filme', list(movie_titles.keys()))
+
+    stars = st.number_input(
+        label='Estrelas',
+        min_value=0,
+        max_value=5,
+        step=1,
     )
+
+    comment = st.text_area('Comentário')
+    
+    if st.button('Cadastrar'):
+        new_review = review_service.create_review(
+            movie=movie_titles[selected_movie_title], # atraves do nome pegamos o id 
+            stars=stars,
+            comment=comment,
+        )
+        if new_review:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar avaliação. verifique os campos.')
---
+++ reviews/repository.py
@@ -0,0 +1,44 @@
+import os
+import requests
+import streamlit as st
+from dotenv import load_dotenv
+
+from login.service import logout
+
+
+load_dotenv()
+
+
+class ReviewRepository:
+
+    def __init__(self):
+        self.__base_url = os.environ['BASE_URL']
+        self.__reviews_url = f'{self.__base_url}reviews/'
+        self.__headers = {
+            'authorization': f'Bearer {st.session_state.token}' 
+        }
+    
+    def get_reviews(self):
+        response = requests.get(
+            self.__reviews_url,
+            headers=self.__headers,
+        )
+        if response.status_code == 200:
+            return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
+        raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
+
+    def create_review(self, review):
+        response = requests.post(
+            self.__reviews_url,
+            headers=self.__headers,
+            data=review,
+        )
+        if response.status_code == 201:
+            return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
+        raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
